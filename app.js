69627c658ed9e6b544142b3c2254b3885862b943
Arpan Mukherjee <arpan.mukherjee.pro@gmail.com>
2019-10-05 03:57:19 +0530

    CORS Support added

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
