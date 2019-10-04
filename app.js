101239186b5185094316c6a7a15979cda7a63752
Arpan Mukherjee <arpan.mukherjee.pro@gmail.com>
2019-10-04 17:22:21 +0530

    Code Refactor

--- app.js
+++ app.js
@@ -58,42 +58,7 @@ io.on('connection', socketioJwt.authorize({
     
     await joinAllDeviceChannelForUser(user['userid'], socket);
 
-    socket.on('get_device_list', async () => {
-
-      try {
-        const { rows } = await pool.query(`SELECT deviceid, devicename from device where userid = $1`, [user['userid']]);
-        if(rows && rows.length > 0) {
-          socket.emit('get_device_list', rows);
-        }
-      } catch(ex) {
-        console.log(ex);
-      }
-    }); 
-
-    socket.on('get_initial_data', async () => {
-      console.log("hello");
-      try{
-        const { rows } = await pool.query(
-          `select 
-            device.deviceid, 
-            monitor.status, 
-            monitor.load, 
-            monitor.time 
-          from 
-            monitor
-            inner join device on device.deviceid = monitor.deviceid 
-            inner join users on users.userid = device.userid
-          where users.userid = $1`, [user['userid']]);
-
-        console.log(rows);
-        
-        socket.emit('get_initial_data', rows);
-
-      } catch(ex) {
-
-      }
-    })
-
+    monitorHandler(socket, pool);
 
 });
 
--- events/monitorHandler.js
+++ events/monitorHandler.js
@@ -1,12 +1,40 @@
-module.exports = function (socket, db) {
+module.exports = function (socket, pool) {
     // registration related behaviour goes here...
-    socket.on('ready', function (data) {
-        // do stuff
-        db.on('notification', function(msg) {
-            let payload = JSON.parse(msg.payload);
-            
-        });
-        
-        client.query('LISTEN new_monitor_entry');
-    });
+    const user = socket.decoded_token.userDetails;
+    
+    socket.on('get_device_list', async () => {
+
+        try {
+          const { rows } = await pool.query(`SELECT deviceid, devicename from device where userid = $1`, [user['userid']]);
+          if(rows && rows.length > 0) {
+            socket.emit('get_device_list', rows);
+          }
+        } catch(ex) {
+          console.log(ex);
+        }
+      }); 
+  
+      socket.on('get_initial_data', async () => {
+        console.log("hello");
+        try{
+          const { rows } = await pool.query(
+            `select 
+              device.deviceid, 
+              monitor.status, 
+              monitor.load, 
+              monitor.time 
+            from 
+              monitor
+              inner join device on device.deviceid = monitor.deviceid 
+              inner join users on users.userid = device.userid
+            where users.userid = $1`, [user['userid']]);
+  
+          console.log(rows);
+          
+          socket.emit('get_initial_data', rows);
+  
+        } catch(ex) {
+  
+        }
+      })
 };
